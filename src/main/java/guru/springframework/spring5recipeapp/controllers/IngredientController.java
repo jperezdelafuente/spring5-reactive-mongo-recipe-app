f07ccf0b5129ec4983a42abc2e55194cd2520129
jperezdelafuente <jesusperez82@gmail.com>
2018-03-30 20:26:17 +0200

    refactoring to reactive types for thymeleaf in reactive mode (recipe and ingredient controllers)

--- src/main/java/guru/springframework/spring5recipeapp/controllers/IngredientController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/IngredientController.java
@@ -34,7 +34,7 @@ public class IngredientController {
         log.debug("Getting ingredient list for recipe id: " + recipeId);
 
         // use command object to avoid lazy load errors in Thymeleaf.
-        model.addAttribute("recipe", recipeService.findCommandById(recipeId).block());
+        model.addAttribute("recipe", recipeService.findCommandById(recipeId));
 
         return "recipe/ingredient/list";
     }
@@ -42,7 +42,7 @@ public class IngredientController {
     @GetMapping("recipe/{recipeId}/ingredient/{id}/show")
     public String showRecipeIngredient(@PathVariable String recipeId,
                                        @PathVariable String id, Model model){
-        model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(recipeId, id).block());
+        model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(recipeId, id));
         return "recipe/ingredient/show";
     }
 
@@ -61,15 +61,7 @@ public class IngredientController {
         //init uom
         ingredientCommand.setUnitOfMeasure(new UnitOfMeasureCommand());
 
-        log.info("IngredientController - Before: Mono mono =unitOfMeasureService.listAllUoms().collectList();");
-        Mono mono =unitOfMeasureService.listAllUoms().collectList();
-        log.info("IngredientController - After: Mono mono =unitOfMeasureService.listAllUoms().collectList();");
-
-        log.info("IngredientController - Before: List<UnitOfMeasureCommand> unitOfMeasureCommandList = (List) mono.block();");
-        List<UnitOfMeasureCommand> unitOfMeasureCommandList = (List) mono.block();
-        log.info("IngredientController - After: List<UnitOfMeasureCommand> unitOfMeasureCommandList = (List) mono.block();");
-
-        model.addAttribute("uomList", unitOfMeasureCommandList);
+        model.addAttribute("uomList", unitOfMeasureService.listAllUoms());
 
         return "recipe/ingredient/ingredientform";
     }
@@ -77,9 +69,9 @@ public class IngredientController {
     @GetMapping("recipe/{recipeId}/ingredient/{id}/update")
     public String updateRecipeIngredient(@PathVariable String recipeId,
                                          @PathVariable String id, Model model){
-        model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(recipeId, id).block());
+        model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(recipeId, id));
 
-        model.addAttribute("uomList", unitOfMeasureService.listAllUoms().collectList().block());
+        model.addAttribute("uomList", unitOfMeasureService.listAllUoms());
         return "recipe/ingredient/ingredientform";
     }
 
