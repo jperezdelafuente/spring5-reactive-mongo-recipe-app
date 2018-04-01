e2c8b80b2104751ea9665fd3927994409909e4dc
jperezdelafuente <jesusperez82@gmail.com>
2018-04-01 20:13:10 +0200

    adding handleNotFound exception

--- src/main/java/guru/springframework/spring5recipeapp/controllers/ControllerExceptionHandler.java
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/ControllerExceptionHandler.java
@@ -3,37 +3,24 @@ package guru.springframework.spring5recipeapp.controllers;
 import guru.springframework.spring5recipeapp.exceptions.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.thymeleaf.exceptions.TemplateInputException;
 
 @Slf4j
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-//    @ResponseStatus(HttpStatus.NOT_FOUND)
-//    @ExceptionHandler(NotFoundException.class)
-//    public ModelAndView handleNotFound(Exception exception){
-//        log.error("Handling not found exception: {}", exception.getMessage());
-//
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.setViewName("errors/404error");
-//        modelAndView.addObject("exception", exception);
-//
-//        return modelAndView;
-//    }
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({NotFoundException.class, TemplateInputException.class})
+    public String handleNotFound(Exception exception, Model model){
+        log.error("Handling not found exception: {}", exception.getMessage());
 
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    @ExceptionHandler(NumberFormatException.class)
-//    public ModelAndView handleNumberFormat(Exception exception){
-//
-//        log.error("Handling Number Format Exception: {}", exception.getMessage());
-//
-//        ModelAndView modelAndView = new ModelAndView();
-//
-//        modelAndView.setViewName("errors/400error");
-//        modelAndView.addObject("exception", exception);
-//
-//        return modelAndView;
-//    }
+        model.addAttribute("exception", exception);
+
+        return "errors/404Error";
+    }
+
 }
